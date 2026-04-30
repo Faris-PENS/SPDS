c56d4ca936a0f14f645308d01e44503a1162ba25
afif <farisafif340@gmail.com>
2026-04-30 15:54:58 +0700

    add some checking function

--- lib/data/datasource/remote/supabase/share_dev_supabase.dart
+++ lib/data/datasource/remote/supabase/share_dev_supabase.dart
@@ -11,17 +11,7 @@ class SharedevSupabase {
       final username = await LocalSession.loadSessionuser();
       if (username == null) return true;
 
-      final user = await _client
-          .from('user')
-          .select('id')
-          .eq('user', username)
-          .maybeSingle();
-
-      if (user == null) {
-        debugPrint("User tidak ditemukan");
-        return true;
-      }
-
+    
       final res = await _client
           .from('DeviceHW')
           .select('id')
@@ -32,14 +22,13 @@ class SharedevSupabase {
         return true;
       }
 
-      final userId = user['id'];
       final deviceId = res['id'];
 
       await _client
           .from('sharedev')
           .delete()
-          .eq('device_id', deviceId)
-          .eq('user_id', userId);
+          .eq('HWID', deviceId)
+          .eq('user_id', username);
 
       debugPrint("shared device deleted");
       return false;
@@ -55,18 +44,10 @@ class SharedevSupabase {
 
       if (username == null) return [];  
 
-      final user = await _client
-          .from('user')
-          .select('id')
-          .eq('user', username)
-          .maybeSingle();
-
-      if (user == null) return [];
-
       final res = await _client
           .from('sharedev')
           .select('DeviceHW(HWID,tempat)')
-          .eq('user_id', user['id']);
+          .eq('user_id', username);
 
       final List<Map<String, dynamic>> devices = [];
 
@@ -83,4 +64,84 @@ class SharedevSupabase {
       return [];
     }
   }
+
+  Future<bool> isRegistered({required String hwid}) async {
+    try {
+      final username = await LocalSession.loadSessionuser();
+      if (username == null) return true;
+
+      final device = await _client
+          .from('DeviceHW')
+          .select('id')
+          .eq('HWID', hwid)
+          .maybeSingle();
+
+      if (device == null) {
+        debugPrint("Device tidak ditemukan");
+        return false;
+      }
+    
+      return true;
+    } catch (e) {
+      debugPrint("isShared error: $e");
+      return false;
+    }
+  }
+
+
+  Future<bool> isShared({required String hwid}) async {
+    try {
+      final username = await LocalSession.loadSessionuser();
+      if (username == null) return true;
+
+      final device = await _client
+          .from('DeviceHW')
+          .select('id')
+          .eq('HWID', hwid)
+          .maybeSingle();
+
+      if (device == null) {
+        print("Device tidak ditemukans");
+        return false;
+      }
+      final deviceId = device['id'];
+      print(  "deviceawdawdawdawdwadawdawdId: $deviceId, username: $username");
+
+      final checkowner = await _client
+    .from('DeviceHW')
+    .select()
+    .eq('id', deviceId) 
+    .maybeSingle();
+
+    if (checkowner == null) {
+      return false;
+    }
+
+    final shared = await _client
+    .from('sharedev')
+    .select()
+    .eq('HWID', deviceId)
+    .eq('user_id', username)
+    .maybeSingle();
+
+    if (shared != null) {
+  return true;
+}
+
+
+  await _client.from('sharedev').insert({
+    'HWID': deviceId,
+    'user_id': username,
+  });
+
+return false;
+   
+    } catch (e) {
+      debugPrint("isShared error: $e");
+      return true;
+    }
+  }
+
+
+
 }
